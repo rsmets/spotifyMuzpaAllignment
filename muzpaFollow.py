6b111d6dd3ab166ad1b9bda1f6f34881709c4712
Ray Smets <rayjsmets@gmail.com>
2019-11-28 11:50:08 -0800

    readme

--- muzpaFollow.py
+++ muzpaFollow.py
@@ -1,31 +1,8 @@
 import requests, json, sys, time, random
 
-headers = {
-    'Cookie': "SESS=6fb6428329bddc27c87f31871270f6c715bc53",
-    'Accept': "*/*",
-    'Cache-Control': "no-cache",
-    'Host': "srv.muzpa.com",
-    'Accept-Encoding': "gzip, deflate",
-    'Connection': "keep-alive",
-    'cache-control': "no-cache"
-    }
-
-headersOld = {
-    'Cookie': "SESS=6fb6428329bddc27c87f31871270f6c715bc53",
-    # 'Content-Type': "text/plain",
-    'Accept': "*/*",
-    'Cache-Control': "no-cache",
-    'Host': "srv.muzpa.com",
-    'Accept-Encoding': "gzip, deflate",
-    'Content-Length': "40",
-    'Connection': "keep-alive",
-    'cache-control': "no-cache"
-    }
-
 def search(aristName):
     url = "https://srv.muzpa.com/a/ms/media/search"
 
-    # querystring = {"mp3prefered":"false","page":"0","popular_order":"false","text":"%5C.Dj+Tennis%5Cs"}
     querystring = {"mp3prefered":"false","page":"0","popular_order":"false","text":aristName}
 
     response = requests.request("GET", url, headers=headers, params=querystring)
@@ -43,7 +20,6 @@ def search(aristName):
             tracks = album['tracks']
             if tracks:
                 for track in tracks:
-                    # if artistName in track['filename']:
                     # print track['artist']
                     if matchArtist(track['artist'], aristName) > -1:
                         # print track['artist']
@@ -75,7 +51,6 @@ def sub(id):
 
     url = "https://srv.muzpa.com/a/ms/artists_subscribes"
 
-    # payload = "{\"artist_id\":76849,\"revision\":\"1334161\"}"
     payload = "{\"artist_id\":" + str(id) + ",\"revision\":\"1334161\"}"
 
     response = requests.request("POST", url, data=payload, headers=headers)
@@ -102,9 +77,19 @@ def readFileAndSearchAndFollow(fileInput):
             line = fp.readline()
 
 
-# search('Dj Tennis')
-
 s = sys.argv[1]
+token = sys.argv[2]
+
+headers = {
+    'Cookie': token,
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Host': "srv.muzpa.com",
+    'Accept-Encoding': "gzip, deflate",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+    }
+
 # searchAndFollow(s)
 readFileAndSearchAndFollow(s)
 
